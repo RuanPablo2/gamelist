c8d0f76ac5c6aa6a15aa62aab0183e6a47f73b48
Ruan Pablo <ruan.pablo2002@outlook.com.br>
2025-01-23 02:06:45 -0300

    properties, Game, ORM

---
+++ src/main/java/com/RuanPablo2/gamelist/entities/Game.java
@@ -0,0 +1,28 @@
+package com.RuanPablo2.gamelist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+}
